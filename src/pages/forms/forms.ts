c088001d04b3fd89ed4c6e7b186734a3a17106ac
Nicolas Troles <nicolastroles95@gmail.com>
2019-02-03 23:28:48 -0200

    [Form] Ajuste de Select e validação de campos.

--- src/pages/forms/forms.ts
+++ src/pages/forms/forms.ts
@@ -9,11 +9,14 @@ import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms'
 export class FormsPage {
 
 	credentialsForm: FormGroup;
+	myUfs = [];
 
 	constructor(public navCtrl: NavController,
 		public navParams: NavParams,
 		private formBuilder: FormBuilder,
 		private alertCtrl: AlertController) {
+
+		this.myUfs = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"]
 
 		this.credentialsForm = this.formBuilder.group({
 			enderecoComercial: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
@@ -21,15 +24,24 @@ export class FormsPage {
 			complemento: [''],
 			cep: ['', Validators.compose([Validators.required, Validators.minLength(9)])],
 			cidade: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-			uf: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+			uf: [''],
 			telComecial: ['', Validators.compose([Validators.required, Validators.minLength(14)])],
 			telCelular: ['', Validators.compose([Validators.minLength(15)])],
 			email: ['', Validators.compose([Validators.maxLength(70), Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'), Validators.required])]
-			
 		});
 	}
 
+	onSelectChange(selectedValue: Number) {
+		// setar UF de form
+	}
+
 	save() {
+		// Aqui pode-se criar validações específicas para o preenchimento incorreto dos campos, 
+		// ressaltando uma mensagem no campo para o usuário como "Este campo deve possuir pelo menos 3 caracteres"
+
+		// Não sei se o ionic possui, mas no Angular Material possui um componente chamado ng-Message que 
+		// avisa o preenchimento incorreto de cada campo em cada situação.
+
 		let alert = this.alertCtrl.create({
 			title: 'Cadastro Realizado com Sucesso',
 			buttons: ['Ok']
